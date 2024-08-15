8f6d7f8cba841f1b393e0904b4944d8ae4b4bc6d
danil <danil@NBLK-WAX9X-26716e8e.localdomain>
2024-08-15 23:54:57 +0500

    last hope

--- src/api/userAPI.js
+++ src/api/userAPI.js
@@ -8,7 +8,7 @@ export const getUserByWallet = async (wallet) => {
 export const registerUser = async (location, invitedId, isPremium, telegramId, telegramName, wallet) => {
     const data = await $host.post(`User/register`, {
         location: String(location),
-        invitedId: JSON.stringify(invitedId) || JSON.stringify(null),
+        invitedId: JSON.parse(invitedId) || JSON.parse(null),
         isPremium: Boolean(isPremium),
         telegramId: String(telegramId),
         telegramName: String(telegramName),
